9fdea9fb2a56df3facf2f2ed3aaa01b143e92f18
Heartless Clown <97714114+heartless-clown@users.noreply.github.com>
2022-07-04 18:04:14 +0300

    feat(app/react-app) add bazel support

--- app/react-app/src/react-app-env.d.ts
+++ app/react-app/src/react-app-env.d.ts
@@ -1,1 +1,15 @@
-/// <reference types="react-scripts" />
+/// <reference types="react" />
+/// <reference types="react-dom" />
+
+// Fixes the issue with importing logo.svg when building
+// IDK, why react-scripts/lib/react-app.d.ts not working
+declare module '*.svg' {
+    import * as React from 'react';
+
+    export const ReactComponent: React.FunctionComponent<React.SVGProps<
+        SVGSVGElement
+        > & { title?: string }>;
+
+    const src: string;
+    export default src;
+}
